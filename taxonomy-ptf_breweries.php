3af1ebd1e485c032d8fcdea17db357b15bdde27e
Daniel Olson <emaildano@gmail.com>
2020-02-10 16:46:11 -0500

    Removed code for testing

--- taxonomy-ptf_breweries.php
+++ taxonomy-ptf_breweries.php
@@ -23,7 +23,6 @@
 						</a> 
 						<span class="tap-list-name"><?php echo $wp_query->queried_object->name; ?></span>
 					</h2>
-					<span class="origin">Origin: <strong><?php echo $beersByBrew[0]->getOrigin(); ?></strong></span>
 				</div>
 				<ul class="the-tap-list">
 				
