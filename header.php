158583bfc9071eedc022c1709a0b6aad56d5b0c9
Daniel Olson <emaildano@gmail.com>
2020-02-11 15:02:55 -0500

    added facebook link icon

--- header.php
+++ header.php
@@ -19,6 +19,14 @@
 <?php } else { ?>
 <body <?php body_class(); ?> data-id="<?php echo $wp_query->post->ID; ?>">
 <?php } ?>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=404063009649497";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 	<div id="container">
 		<header id="main-header">
 			<div class="page-container">
